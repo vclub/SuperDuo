432714e9451e3266b2f738236eaa875684d127fc
vclub <vclub@126.com>
2015-11-22 23:16:57 +0800

    local

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by yehya khaled on 2/27/2015.
@@ -33,7 +34,7 @@ public class PagerFragment extends Fragment
         for (int i = 0;i < NUM_PAGES;i++)
         {
             Date fragmentdate = new Date(System.currentTimeMillis()+((i-2)*86400000));
-            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
             viewFragments[i] = new MainScreenFragment();
             viewFragments[i].setFragmentDate(mformat.format(fragmentdate));
         }
@@ -87,7 +88,7 @@ public class PagerFragment extends Fragment
                 Time time = new Time();
                 time.setToNow();
                 // Otherwise, the format is just the day of the week (e.g "Wednesday".
-                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
                 return dayFormat.format(dateInMillis);
             }
         }
